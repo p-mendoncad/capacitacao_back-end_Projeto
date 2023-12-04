58d53223452f85f2eed02662edcaaf0e9dff0060
p-mendoncad <125631065+p-mendoncad@users.noreply.github.com>
2023-12-04 14:54:16 -0300

    Capacitação Back End - 6

--- api/index.js
+++
@@ -1,8 +0,0 @@
-let cep = 36045440
-let endpoint = `https://viacep.com.br/ws/${cep}/json`;
-let response =  fetch(endpoint, {
-    method: "GET"
-})
-.then(async (res) => {
-console.log(await res.json());
-});
